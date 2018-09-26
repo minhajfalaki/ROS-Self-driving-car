51eb1e51a698cf0840d8e2e97b5b4365dbfda920
minhajfalaki <minhajfalaki@gmail.com>
2018-09-26 11:44:16 +0530

    Update and rename alexnet.py to model.py

--- model.py
+++ model.py
@@ -5,14 +5,13 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
 
-def alexnet(width, height, lr):
+def model(width, height, lr):
     network = input_data(shape=[None, width, height, 3], name='input')
     network = conv_2d(network, 96, 11, strides=4, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
     network = conv_2d(network, 256, 5, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
-    print "alex1"
     network = local_response_normalization(network)
     network = conv_2d(network, 384, 3, activation='relu')
     network = conv_2d(network, 384, 3, activation='relu')
